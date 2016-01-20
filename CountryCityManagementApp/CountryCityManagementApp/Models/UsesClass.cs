1f916714c88c580ee06fb3bf4383886abca10ac7
aveek <nsaveek@gmail.com>
2016-01-20 13:31:08 +0600

    new class added in the models

---
+++ CountryCityManagementApp/CountryCityManagementApp/Models/UsesClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountryCityManagementApp.Models
+{
+    public class UsesClass
+    {
+        //This class is designed to get the necessary outputs that does not required any object/s to initialize
+    }
+}
